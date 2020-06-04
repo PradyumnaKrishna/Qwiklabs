de8e86502f3cbd0357141de07c26a4b089c85ab6
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2020-06-05 00:29:42 +0530

    Update script.sh

--- Google Cloud Computing Foundations Speedrun 3/VPC Networking Fundamentals/script.sh
+++ Google Cloud Computing Foundations Speedrun 3/VPC Networking Fundamentals/script.sh
@@ -3,5 +3,5 @@ gcloud init < a
 
 gcloud compute networks create mynetwork --subnet-mode=auto --bgp-routing-mode=regional
 
-gcloud compute instances create mynet-us-vm --zone=us-central1-c --machine-type=n1-standard-1 --network=mynetwork &
+gcloud compute instances create mynet-us-vm --zone=us-central1-c --machine-type=n1-standard-1 --network=mynetwork
 gcloud compute instances create mynet-eu-vm --zone=europe-west1-c --machine-type=n1-standard-1 --network=mynetwork
