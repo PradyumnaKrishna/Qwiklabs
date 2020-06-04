a4528cfe9c2296990df3a063a805d6062a4cf2e6
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2020-06-05 00:29:26 +0530

    Update script.sh

--- Google Cloud Computing Foundations Speedrun 3/Multiple VPC Networks/script.sh
+++ Google Cloud Computing Foundations Speedrun 3/Multiple VPC Networks/script.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 gcloud init < a.txt
 
-(gcloud compute networks create managementnet --subnet-mode=custom ;
-gcloud compute networks create privatenet --subnet-mode=custom)
+gcloud compute networks create managementnet --subnet-mode=custom
+gcloud compute networks create privatenet --subnet-mode=custom
 
-(gcloud compute networks subnets create managementsubnet-us --network=managementnet --region=us-central1 --range=10.130.0.0/20 &
-gcloud compute networks subnets create privatesubnet-us --network=privatenet --region=us-central1 --range=172.16.0.0/24 &
-gcloud compute networks subnets create privatesubnet-eu --network=privatenet --region=europe-west1 --range=172.20.0.0/20)
+gcloud compute networks subnets create managementsubnet-us --network=managementnet --region=us-central1 --range=10.130.0.0/20
+gcloud compute networks subnets create privatesubnet-us --network=privatenet --region=us-central1 --range=172.16.0.0/24
+gcloud compute networks subnets create privatesubnet-eu --network=privatenet --region=europe-west1 --range=172.20.0.0/20
 
-gcloud compute firewall-rules create managementnet-allow-icmp-ssh-rdp --network=managementnet --allow tcp:22,tcp:3389,icmp &
-gcloud compute firewall-rules create privatenet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=privatenet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0 &
+gcloud compute firewall-rules create managementnet-allow-icmp-ssh-rdp --network=managementnet --allow tcp:22,tcp:3389,icmp
+gcloud compute firewall-rules create privatenet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=privatenet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0
 
-gcloud compute instances create managementnet-us-vm --zone=us-central1-c --machine-type=f1-micro --network=managementnet --subnet=managementsubnet-us &
-gcloud compute instances create privatenet-us-vm --zone=us-central1-c --machine-type=n1-standard-1 --subnet=privatesubnet-us &
+gcloud compute instances create managementnet-us-vm --zone=us-central1-c --machine-type=f1-micro --network=managementnet --subnet=managementsubnet-us
+gcloud compute instances create privatenet-us-vm --zone=us-central1-c --machine-type=n1-standard-1 --subnet=privatesubnet-us
 gcloud compute instances create vm-appliance --zone=us-central1-c --machine-type=n1-standard-4 \
       --network-interface network=privatenet,subnet=privatesubnet-us \
       --network-interface network=managementnet,subnet=managementsubnet-us \
