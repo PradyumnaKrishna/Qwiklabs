e510f47f61004465c81134f73cc155e867d3532a
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2020-07-06 13:39:47 +0530

    ADDED QUEST

---
+++ Quests/Intro to ML: Language Processing/Cloud Natural Language API: Qwik Start/script.sh
@@ -0,0 +1,36 @@
+# !/bin/sh
+
+###################################################################################################################
+#                                                   MIT License                                                   # 
+#                                                                                                                 #
+# Copyright (c) 2022 Pradyumna Krishna | Abhinandan Arya                                                          #
+#                                                                                                                 #
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated    #
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation #
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,    #
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:          #
+#                                                                                                                 #
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions   #
+# of the Software.                                                                                                #
+#                                                                                                                 #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED   #
+# TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   #
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF   #
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        #
+# DEALINGS IN THE SOFTWARE.                                                                                       #
+###################################################################################################################
+
+
+gcloud init < a
+
+export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value core/project)
+
+gcloud iam service-accounts create my-natlang-sa \
+    --display-name "my natural language service account"
+
+gcloud iam service-accounts keys create key.json \
+    --iam-account my-natlang-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+
+export GOOGLE_APPLICATION_CREDENTIALS="key.json"
+
+gcloud compute scp result.json linux-instance:~/result.json
---
+++ Quests/Intro to ML: Language Processing/README.md
@@ -0,0 +1,21 @@
+# Intro to ML: Language Processing
+
+There are 5 Steps in this [**Quest**](https://www.qwiklabs.com/quests/82)
+There are cheat sheet and script available for several labs
+
+**[How to use Scripts and Cheatsheet](/HOW-TO.md)**
+
+ - **1th LAB : [AI Platform: Qwik Start](https://www.qwiklabs.com/focuses/581?parent=catalog)**
+	- **Testing Needed**
+
+ - **2nd LAB : [Google Cloud Speech API: Qwik Start](https://www.qwiklabs.com/focuses/588?parent=catalog)**
+	- You need to create API key as directed by the lab going at this **[LINK](https://console.cloud.google.com/apis/credentials)**
+
+ - **3st LAB : [Cloud Natural Language API: Qwik Start](https://www.qwiklabs.com/focuses/582?parent=catalog)**
+	- This lab is working fine. No need to add anything or change
+
+ - **4th LAB : [Entity and Sentiment Analysis with the Natural Language API](https://www.qwiklabs.com/focuses/1843?parent=catalog)**
+	- You need to create API key as directed by the lab going at this **[LINK](https://console.cloud.google.com/apis/credentials)**
+
+ - **5th LAB : [Speech to Text Transcription with the Cloud Speech API](https://www.qwiklabs.com/focuses/2187?parent=catalog)**
+	- **Lab Unavailable**
