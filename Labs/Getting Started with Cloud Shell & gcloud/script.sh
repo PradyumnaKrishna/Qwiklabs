e95804a458947dd228e490c8c8852c3699fe991e
Pradyumna <51270439+PradyumnaKrishna@users.noreply.github.com>
2020-09-15 10:07:06 +0530

    Script: Getting Started with Cloud Shell & gcloud (UPGRADED)

--- Labs/Getting Started with Cloud Shell & gcloud/script.sh
+++ Labs/Getting Started with Cloud Shell & gcloud/script.sh
@@ -20,5 +20,17 @@
 # DEALINGS IN THE SOFTWARE.                                                                                       #
 ###################################################################################################################
 
+# Initialization of Script
 gcloud init < a
-gcloud compute instances create gcelab2 --machine-type n1-standard-2 --zone us-central1-c
+
+# Create a virtual machine with gcloud
+if  gcloud compute instances create gcelab2 --machine-type n1-standard-2 \
+    --zone us-central1-c
+then
+  printf "\n\e[1;96m%s\n\n\e[m" 'Created VM Instance: Checkpoint Completed (1/1)'
+  sleep 2.5
+
+  printf "\n\e[1;92m%s\n\n\e[m" 'Lab Completed'
+fi
+
+gcloud auth revoke --all
